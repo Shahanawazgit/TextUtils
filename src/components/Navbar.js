ee203ae10a32e86f9b284534e204fa00f10ffb01
Shahanawaz <shahanawazkhan579@gmail.com>
2023-09-04 20:06:01 +0530

    active class fixed in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,10 @@
-import React, {useLocation} from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Navbar(props) {
-  
-  let location = useLocation();
-  
+  const location = useLocation();
+
   return (
     <>
       <nav
@@ -30,16 +29,23 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className={`nav-link ${
+                <Link
+                  className={`nav-link ${
                     location.pathname === "/" ? "active" : ""
-                  }`} aria-current="page" to="/">
+                  }`}
+                  aria-current="page"
+                  to="/"
+                >
                   Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className={`nav-link ${
+                <Link
+                  className={`nav-link ${
                     location.pathname === "/about" ? "active" : ""
-                  }`} to="/about">
+                  }`}
+                  to="/about"
+                >
                   {props.aboutText}
                 </Link>
               </li>
